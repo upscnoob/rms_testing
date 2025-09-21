8e9ae4772502de9b609062575b9ccea37db7422f
upscnoob <141261769+upscnoob@users.noreply.github.com>
2025-09-21 17:51:32 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'rms-sorter-cache-v1';
+// Make sure this matches the name of your HTML file
+const urlsToCache = [
+  './',
+  'index.html' 
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
